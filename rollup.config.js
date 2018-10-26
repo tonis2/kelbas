f4ef1e1900399102deff96c5728c3584e94f7857
tonis anton <tonis.anton18@gmail.com>
2018-10-26 13:21:48 +0300

    Add esm modules version bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import uglify from "rollup-plugin-uglify";
+import uglify from "rollup-plugin-uglify"
 
 export default [
   {
@@ -6,8 +6,17 @@ export default [
     plugins: [uglify()],
     output: {
       file: "build/kelbas.min.js",
-      format: "umd",
-      name:"kelbas"
+      format: "cjs",
+      name: "kelbas"
+    }
+  },
+  {
+    input: "./index.js",
+    plugins: [uglify()],
+    output: {
+      file: "build/kelbas.esm.js",
+      format: "es",
+      name: "kelbas"
     }
   }
 ]
